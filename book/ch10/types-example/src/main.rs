44a3aa0daa8e1763646c1a727a49a126155d4f40
Alvin(Xinyao) Sun <lucklyric@gmail.com>
2022-09-14 00:54:32 -0700

    feat(book): ch10 types

--- book/ch10/types-example/src/main.rs
+++ book/ch10/types-example/src/main.rs
@@ -1,3 +1,100 @@
-fn main() {
-    println!("Hello, world!");
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
 }
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
+}
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
+}
+
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> Point<T, U> {
+    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    // generic types
+
+    let number_list = vec![34, 50, 25, 100, 65, 11111];
+
+    let mut largest_n = &number_list[0];
+
+    for number in &number_list {
+        if number > largest_n {
+            largest_n = number;
+        }
+    }
+
+    println!("The largest number is {}", largest_n);
+
+    let i32_list = vec![1, 2, 3, 3, 4, 576];
+
+    let largest_n = largest_i32(&i32_list);
+
+    println!("{}", largest_n);
+
+    let char_list = vec!['a', 'b', 'c', 'd'];
+
+    let largest_char = largest_char(&char_list);
+
+    println!("{}", largest_char);
+
+    let largest_v = largest(&char_list);
+
+    println!("{}", largest_v);
+
+    let both_integer = Point { x: 5, y: 10 };
+
+    let both_float = Point { x: 5.0, y: 10.9 };
+
+    let integar_and_flot = Point { x: 5, y: 10.0 };
+
+    // print all points
+    println!("{}, {}", both_integer.x, both_integer.y);
+    println!("{}, {}", both_float.x, both_float.y);
+    println!("{}, {}", integar_and_flot.x, integar_and_flot.y);
+
+    let p1 = Point { x: 5, y: 10.4 };
+
+    let p3 = p1.mixup(both_integer);
+
+    println!("{}, {}", p3.x, p3.y);
+}
+
+
