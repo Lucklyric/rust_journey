266653bc430ea0ef4c14f3e144f408302e9d4613
Alvin(Xinyao) Sun <asun@AlvinXinyaos-MacBook-Pro.local>
2022-09-27 23:20:18 -0600

    feat(book): ch12 cnt

--- book/ch12/minigrep/src/main.rs
+++ book/ch12/minigrep/src/main.rs
@@ -1,9 +1,20 @@
+use minigrep::Config;
+use std::process;
 use std::{env, fs};
 
 fn main() {
     println!("Hello, world!");
 
-    let args: Vec<String>  = env::args().collect();
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments {err}");
+        process::exit(1);
+    });
+
+    config.run();
+
+    println!("{},{}", config.query, config.file_path);
 
     let query = &args[1];
 
@@ -14,7 +25,11 @@ fn main() {
 
     let contents = fs::read_to_string(&file_path).expect("Should have been able to read the file");
 
-    println!("With text:\n{contents}")
+    println!("With text:\n{contents}");
 
-
+    // stage two
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
